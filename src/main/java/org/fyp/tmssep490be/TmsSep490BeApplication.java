39df5f46c9e73b6bbd2fd535b2dfef4080dadf59
mthangtr <cccccc9712@gmail.com>
2025-12-06 18:08:38 +0700

    init setup

---
+++ src/main/java/org/fyp/tmssep490be/TmsSep490BeApplication.java
@@ -0,0 +1,13 @@
+package org.fyp.tmssep490be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmsSep490BeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmsSep490BeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/fyp/tmssep490be/TmsSep490BeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fyp.tmssep490be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmsSep490BeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
